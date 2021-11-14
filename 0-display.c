0d01618542f6e6b8e425bfc85a5d5b0bfebea05c
mr.b <bruno932151@gmail.com>
2021-11-14 18:08:26 -0500

    task 0 was creted

---
+++ 0-display.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+ * display_complex_number - displays the complex numbers
+ * @c: complex
+ */
+void display_complex_number(complex c)
+{
+	char i;
+
+	x = (c.im < 0) ? '-' : '+';
+
+	if (c.im == 0)
+	{
+		printf("%.9g\n", c.re);
+	}
+	else
+	{
+		c.im = (c.im < 0) ? -(c.im) : c.im;
+		if (c.im == 1)
+		{
+			printf("%.9g %c i\n", c.re, i);
+			return;
+		}
+		else if (c.im == -1)
+		{
+			printf("%.9g %c -i\n", c.re, i);
+			return;
+		}
+		printf("%.9g %c %.9gi\n", c.re, i, c.im);
+	}
+}
