a707009ee12cd0544f6263ac70763a9118619633
mr.b <bruno932151@gmail.com>
2021-11-14 19:25:16 -0500

    task 5 was created

---
+++ 0x00-math_complex/5-substraction.c
@@ -0,0 +1,12 @@
+#include "holberto.h"
+/**
+ * substraction - performs the substraction operation to complex numbers
+ * @c1: complex
+ * @c2: complex
+ * @c3: pointer to complex
+ */
+void substraction(complex c1, complex c2, complex *c3)
+{
+	c3->re = c1.re - c2.re;
+	c3->im = c1.im - c2.im;
+}
